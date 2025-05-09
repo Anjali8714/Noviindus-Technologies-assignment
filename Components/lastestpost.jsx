492b4f82e2fc03c8fa7740c390c92eeeec1ad9d4
Anjali8714 <anjalit9714@gmail.com>
2025-05-10 01:13:31 +0530

    latest post section

--- Components/lastestpost.jsx
+++ Components/lastestpost.jsx
@@ -6,7 +6,7 @@ const Lastestpost = () => {
     <div className='bg-white w-full'>
         <h1 className='text-[#86775c] flex  justify-center items-center py-10 text-3xl font-semibold'>Prepare Your Child for a World of Opportunities with Educare</h1>
         <div className='max-w-[1030px] mx-auto'>
-      <Image src='/images/Component 86.jpg' alt='latest post' width={0} height={0} sizes="1000vw" className="w-full h-[500px] object-contain" />
+      <Image src='/images/latestpost.jpg' alt='latest post' width={0} height={0} sizes="1000vw" className="w-full h-[500px] object-contain" />
         </div>
         <div className='bg-[#FFF3E1] text-[#8A1538] flex p-3 justify-center text-lg'>
         <span>Education is the foundation of greatness; let’s build it together</span>
